b3963edcfb65d6d17df305d57084d4fd88603d1e
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-07-24 15:55:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Add your code here
+function submitData(name, email) {
+  let formData = {
+    let name = name;
+    let email = email;
+  }
+  let configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+  }
+  fetch("http://localhost:3000/users", configObj)
+}
