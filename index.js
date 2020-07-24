c1d88f7baed2ef6a7f501e029ee473244c9f837b
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-07-24 16:03:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function submitData(name, email) {
   let formData = {
-    "name": name;
-    "email" = email;
+    "name": name,
+    "email": email;
   }
   let configObj = {
     method: "POST",
